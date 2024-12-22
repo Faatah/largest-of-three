a6fdd1445219067aeb66ddb86ddd8c366194979a
Faatah <guure781@gmail.com>
2024-12-22 13:24:14 +0300

    done exercise for largest-of-three numbers

--- challenge.js
+++ challenge.js
@@ -1,2 +1,13 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let num1=12
+let num2=14
+let num3=16
+
+if (num1>num2 && num1>num3){
+    console.log("The largest number out of the three numbers is "+num1)
+}else if(num2>num1 && num2>num3){
+    console.log("The largest number out of the three numbers is "+num2)
+}else if(num3>num1 && num3>num2){
+    console.log("The largest number out of the three numbers is "+num3)
+}
